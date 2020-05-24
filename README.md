2390622293bf1a770a7433f2f20f62eedecffee5
Joao Portela <jcbastosportela@gmail.com>
2020-05-24 16:01:09 +0200

    Fixed bugs introduced by refratoring

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pyTraceMonitor
 A python version of Weinzierl's TraceMonitor (KNX development)
+
+## deps
+pySerial
+numpy
+termcolor
--- pyTraceMon.py
+++ pyTraceMon.py
@@ -1,5 +1,5 @@
 import serial
-from TMParser import parser
+from TMParser import parser as tm_parser
 import os
 import argparse
 
@@ -14,13 +14,13 @@ def read_map_from_file(path):
     :return: The map dictionary
     :rtype: dict
     """
-    map_dic = {}
+    ret_map_dic = {}
     with open(path) as f:
         for line in f:
             if line.startswith('0x'):
                 (key, val) = line.split(maxsplit=1)
-                map_dic[int(key, 0)] = val
-    return map_dic
+                ret_map_dic[int(key, 0)] = val
+    return ret_map_dic
 
 
 def create_map_from_project(path):
@@ -33,24 +33,25 @@ def create_map_from_project(path):
     :return: The map dictionary
     :rtype: dict
     """
-    map_dic = {}
+    ret_map_dic = {}
 
     for root, dirs, files in os.walk(path):
         for file in files:
-            if file.endswith('.c'):
+            if file.endswith('.c') or file.endswith('.cpp'):
                 print(os.path.join(root, file))
                 with open(os.path.join(root, file), encoding="ISO-8859-1") as f:
                     for num, line in enumerate(f, 1):
                         if 'MODULE_ID' in line and '#define' in line:
                             module_id = eval(line.lstrip().rstrip().split()[2]) << 8 * 2
                         if 'TRACE_TEXT' in line:
-                            text = line[line.find('"') + len('"'):line.rfind('"')].replace('\\t', '\t').replace('\\n',
-                                                                                                                '\n')
-                            map_dic[module_id + num] = text
-    return map_dic
+                            text = line[line.find('"') + len('"'):line.rfind('"')]\
+                                .replace('\\t', '\t')\
+                                .replace('\\n', '\n')
+                            ret_map_dic[module_id + num] = text
+    return ret_map_dic
 
 
-def run_trace_mon(serial_device, serial_baudrate, serial_parity, map_dic):
+def run_trace_mon(serial_device, serial_baudrate, serial_parity, debug_strings_map):
     """
 
     :param serial_device: The serial device to use /dev/tty...
@@ -61,20 +62,20 @@ def run_trace_mon(serial_device, serial_baudrate, serial_parity, map_dic):
     """
     ser = serial.Serial(port=serial_device, baudrate=serial_baudrate, parity=serial_parity)
 
-    com_parser = parser.Parser(map_dic)
+    com_parser = tm_parser.Parser(debug_strings_map)
     while True:
         com_parser.process(ser.read(1))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path', help='Path to the map file or to the project top be parsed', )
-    parser.add_argument('-d', '--device', required=True, help='Path to the serial device connected to the debug UART')
-    parser.add_argument('-b', '--baudrate', default=460800, help='The baudrate of the serial communication of the debug')
-    parser.add_argument('-p', '--parity', default=serial.PARITY_NONE, help='The parity ("N" none, "O" odd, "E" even) of'
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('path', help='Path to the map file or to the project top be parsed', )
+    args_parser.add_argument('-d', '--device', required=True, help='Path to the serial device connected to the debug UART')
+    args_parser.add_argument('-b', '--baudrate', default=460800, help='The baudrate of the serial communication of the debug')
+    args_parser.add_argument('-p', '--parity', default=serial.PARITY_NONE, help='The parity ("N" none, "O" odd, "E" even) of'
                                                                            'the serial communication of the debug')
 
-    args = parser.parse_args()
+    args = args_parser.parse_args()
 
     map_dic = {}
     if os.path.isfile(args.path):
@@ -82,4 +83,5 @@ if __name__ == '__main__':
     else:
         map_dic = create_map_from_project(args.path)
 
-    run_trace_mon(serial_device=args.device, serial_baudrate=args.baudrate, serial_parity=args.parity, map_dic=map_dic)
+    run_trace_mon(serial_device=args.device, serial_baudrate=args.baudrate, serial_parity=args.parity,
+                  debug_strings_map=map_dic)
